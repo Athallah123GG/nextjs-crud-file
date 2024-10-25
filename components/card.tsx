39650f42ab4d278d3489c401a89eb1a80bae6a42
Athallah123GG <amzing545@gmail.com>
2024-10-25 14:50:42 +0700

    d

--- components/card.tsx
+++ components/card.tsx
@@ -1,10 +1,8 @@
 // import React from 'react'
 import Image from 'next/image';
 import { DeleteButton, EditButton } from '@/components/button';
-// import type { Upload } from '@prisma/client'
-// import type { Upload } from '@prisma/client';
-import { Upload } from '@/prisma/generated/client';
-// import type { Upload } from '@/generated/client';
+import type { Upload } from '@prisma/client';
+
 
 
 
